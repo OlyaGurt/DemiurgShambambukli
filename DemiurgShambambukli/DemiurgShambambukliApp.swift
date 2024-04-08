89e9b813acee5f42871ff9a9ea0a7c8f8f336f90
OlgaGurtueva <olga.2000.11@gmail.com>
2024-04-09 06:06:03 +0700

    Initial Commit

---
+++ DemiurgShambambukli/DemiurgShambambukliApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemiurgShambambukliApp.swift
+//  DemiurgShambambukli
+//
+//  Created by Ольга Гуртуева on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct DemiurgShambambukliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
